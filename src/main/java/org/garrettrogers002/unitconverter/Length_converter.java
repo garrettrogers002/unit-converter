950098d540130a6419c0a3dd27455f9bcb7d9e20
Gary <garrett.rogers002@gmail.com>
2026-02-12 10:32:54 -0500

    fix conversion interface and change Length_converter class

--- src/main/java/org/garrettrogers002/unitconverter/Length_converter.java
+++ src/main/java/org/garrettrogers002/unitconverter/Length_converter.java
@@ -3,9 +3,9 @@ package org.garrettrogers002.unitconverter;
 import java.util.List;
 
 public class Length_converter implements conversion {
-    private double value;
-    private String fromUnit;
-    private String toUnit;
+    private final double value;
+    private  final String fromUnit;
+    private final String toUnit;
 
     public Length_converter(double value, String fromUnit, String toUnit) {
         this.value = value;
@@ -14,7 +14,7 @@ public class Length_converter implements conversion {
     }
     @Override 
     public double convert() {
-        double result = 0;
+        double result = (double) 0;
         // length conversion logic here
         List<String> lengthUnits = List.of("meters", "feet", "inches", "centimeters");
         if (!lengthUnits.contains(fromUnit) || !lengthUnits.contains(toUnit)) {
